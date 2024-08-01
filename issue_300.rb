699aa78b81fee9526558a27ce6f5b4a2553c7dc2
Sebastian Cohnen <tisba@users.noreply.github.com>
2024-08-01 08:21:52 +0200

    adds debug output

--- issue_300.rb
+++ issue_300.rb
@@ -7,6 +7,18 @@ gemfile do
   gem "mini_racer", "0.13.0"
 end
 
+require "libv8-node"
+require "rbconfig"
+
+puts "RbConfig::CONFIG['LIBS']: #{RbConfig::CONFIG["LIBS"]}"
+puts "RUBY_VERSION : #{RUBY_VERSION}"
+puts "RUBY_PLATFORM: #{RUBY_PLATFORM}"
+puts "MiniRacer::VERSION: #{MiniRacer::VERSION}"
+puts "MiniRacer::LIBV8_NODE_VERSION: #{MiniRacer::LIBV8_NODE_VERSION}"
+puts "Libv8::Node::VERSION: #{Libv8::Node::VERSION}"
+puts "Libv8::Node::NODE_VERSION: #{Libv8::Node::NODE_VERSION}"
+puts "Libv8::Node::LIBV8_VERSION: #{Libv8::Node::LIBV8_VERSION}"
+
 Thread
   .new do
     ctx = MiniRacer::Context.new
